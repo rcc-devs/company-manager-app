3504d7cd8b849b27ca5f81b6b8e77ca9c958ba74
João <r4lphoficial@gmail.com>
2018-09-24 19:26:46 -0300

    Update main index.php file

---
+++ login/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <!-- Required tags --> 
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login — Gestão de companhias</title>
+
+  <!-- Required CSS -->
+  <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../assets/css/style.css">
+
+</head>
+<body>
+  
+  <div class="container">
+    div.row>
+  </div>  
+
+
+  <!-- Required JS -->
+  <script src="../assets/js/jquery.min.js"></script>
+  <script src="../assets/js/popper.min.js"></script>
+  <script src="../assets/js/bootstrap.js"></script>
+
+</body>
+</html>
