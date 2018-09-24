ada1b623c4cd92c73bdf301abcfbf737e6a824b9
Bruno Martins <b.gabriel.ma@gmail.com>
2018-09-24 23:38:14 +0100

    Optimization code

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
+
 /*
+*
 * RCC - Gerenciamento de companhias
 * Author bmartins@rcc.com
 *
 */
 
 // Verify if session exists
-if(!isset($_SESSION["rcc_companhias_session"])) {
+if (!isset($_SESSION["rcc_companhias_session"])) {
     header('Location: ./login');
-} else if(isset($_SESSION["rcc_companhias_session"])) {
+} else if (isset($_SESSION["rcc_companhias_session"])) {
     header('Location: ./homepage');
 }
 
