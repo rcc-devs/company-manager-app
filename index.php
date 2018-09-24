81aee27aec2d66f76b23a4bb2c9b021af16b2a10
Bruno Martins <b.gabriel.ma@gmail.com>
2018-09-24 23:31:27 +0100

    Adding session system and redirect

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php echo 'test' ?>; 
+<?php
+/*
+* RCC - Gerenciamento de companhias
+* Author bmartins@rcc.com
+*
+*/
+
+// Verify if session exists
+if(!isset($_SESSION["rcc_companhias_session"])) {
+    header('Location: ./login');
+} else if(isset($_SESSION["rcc_companhias_session"])) {
+    header('Location: ./homepage');
+}
+
+?> 
