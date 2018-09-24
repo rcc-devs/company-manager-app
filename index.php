f3312ac554ea361c486904066f635d1e205fba65
Bruno Martins <b.gabriel.ma@gmail.com>
2018-09-25 00:03:17 +0100

    Optimization code

--- index.php
+++ index.php
@@ -10,8 +10,6 @@
 // Verify if session exists
 if (!isset($_SESSION['rcc_companhias_session'])) {
     header('Location: ./login');
-} else if (isset($_SESSION['rcc_companhias_session'])) {
+} elseif (isset($_SESSION['rcc_companhias_session'])) {
     header('Location: ./homepage');
 }
-
-?>
