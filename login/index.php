b5933314abac888700dc594faedf41abc556bd17
João <r4lphoficial@gmail.com>
2018-09-24 20:14:27 -0300

    Edit index.php file and style.css

--- login/index.php
+++ login/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 
-  <!-- Required tags --> 
+  <!-- Required tags -->
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,8 +14,32 @@
 
 </head>
 <body>
-  
-  
+
+  <img src="../assets/img/logo.png" class="d-block mx-auto my-5" alt="">
+  <div class="container">
+    <div class="d-flex justify-content-center">
+        <form>
+            <div class="form-group">
+                <input type="email" class="form-control" placeholder="Usuário">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="Senha">
+            </div>
+            <button type="submit" class="btn btn-lg btn-block btn-lgn mb-2">Login</button>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group form-check">
+                        <input type="checkbox" class="form-check-input" id="Check">
+                        <label class="form-check-label text-white" for="Check">Permanecer conectado</label>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <a href="#" class="text-white">Esqueceu sua senha?</a>
+                </div>
+            </div>
+        </form>
+      </div>
+  </div>
 
 
   <!-- Required JS -->
