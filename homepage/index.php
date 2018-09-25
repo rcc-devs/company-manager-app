68184bc4cef1b179538a5404781e80248f7871c0
Bruno Martins <b.gabriel.ma@gmail.com>
2018-09-25 21:31:32 +0100

    Divide css for each page. Create homepage-style.css file and configure it to Wire's work

---
+++ homepage/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required tags -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login — Gestão de companhias</title>
+
+    <!-- Required CSS -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/homepage-style.css">
+</head>
+<body>
+    <header>header</header>
+    <main>main</main>
+    <footer>footer</footer>
+</body>
+</html>
