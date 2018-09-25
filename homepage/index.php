a53aefe8274540be0ebab07fd07bf2554cc9baaf
Bruno Martins <b.gabriel.ma@gmail.com>
2018-09-25 21:35:14 +0100

    Write some instructions to wire's work. Update homepage-style.css file

--- homepage/index.php
+++ homepage/index.php
@@ -12,8 +12,17 @@
     <link rel="stylesheet" href="../assets/css/homepage-style.css">
 </head>
 <body>
-    <header>header</header>
-    <main>main</main>
-    <footer>footer</footer>
+    <header>
+        <!-- Espaço para a navbar -->
+        <h6>Navbar</h6>
+    </header>
+    <main>
+        <!-- Todo o conteúdo exceto navbar e footer -->
+        <h6>Content</h6>
+    </main>
+    <footer>
+        <!-- Espaço para o footer -->
+        <h6>Footer</h6>
+    </footer>
 </body>
 </html>
