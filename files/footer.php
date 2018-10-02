8eee07af6869942bfb3df7c716a247d6f8eb41c6
João <r4lphoficial@gmail.com>
2018-10-01 21:22:42 -0300

    Updated file (footer.php)

--- files/footer.php
+++ files/footer.php
@@ -1,6 +1,12 @@
 <footer class="main-footer">
-    
-
-    
-
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-2">A</div>
+            <div class="col-lg-2">B</div>
+            <div class="col-lg-2">C</div>
+            <div class="col-lg-2">D</div>
+            <div class="col-lg-2">E</div>
+            <div class="col-lg-2">F</div>
+        </div>
+    </div>
 </footer>
