9eb3f11e2b8697dcb54366e429b2bbe2ede9f8fc
João <r4lphoficial@gmail.com>
2018-10-02 17:34:44 -0300

    Updated files

--- files/footer.php
+++ files/footer.php
@@ -1,12 +1,62 @@
 <footer class="main-footer">
     <div class="container">
         <div class="row">
-            <div class="col-lg-2">A</div>
-            <div class="col-lg-2">B</div>
-            <div class="col-lg-2">C</div>
-            <div class="col-lg-2">D</div>
-            <div class="col-lg-2">E</div>
-            <div class="col-lg-2">F</div>
+            <div class="col-lg-2">
+                <div class="back">
+                    <a href="" class="isDisabled">
+                        <i class="fas fa-arrow-left"></i>
+                        Início (atual)
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <h6>Sobre nós</h6>
+                <hr class="my-3">
+                <ul class="list-unstyled">
+                    <li><a href="">A companhia</a></li>
+                    <li><a href="">Documentos</a></li>
+                    <li><a href="">Membros</a></li>
+                    <li><a href="">Visão geral</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2">
+                <h6>Links Úteis</h6>
+                <hr class="my-3">
+                <ul class="list-unstyled">
+                    <li><a href="">Aulas</a></li>
+                    <li><a href="">Eventos</a></li>
+                    <li><a href="">Membros</a></li>
+                    <li><a href="">Projetos</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2">
+                <h6>Estatísticas</h6>
+                <hr class="my-3">
+                <ul class="list-unstyled">
+                    <li><a href="">Auditoria</a></li>
+                    <li><a href="">Gratificações</a></li>
+                    <li><a href="">Promoções</a></li>
+                    <li><a href="">Opiniões</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2">
+                <h6>Suporte</h6>
+                <hr class="my-3">
+                <ul class="list-unstyled">
+                    <li><a href="">Contato</a></li>
+                    <li><a href="">FAQ</a></li>
+                    <li><a href="">Reportar bugs</a></li>
+                    <li><a href="">Sugerir algo</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2">
+                <div class="forth">
+                    <a href="">
+                        Administração
+                        <i class="fas fa-arrow-right"></i>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
